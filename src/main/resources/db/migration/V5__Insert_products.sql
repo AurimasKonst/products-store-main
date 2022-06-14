17cfc168092419d023a9f33a9dbd52de5c15be1a
AurimasKonst <97383066+AurimasKonst@users.noreply.github.com>
2022-06-14 21:35:34 +0300

    Flyway2

--- src/main/resources/db/migration/V5__Insert_products.sql
+++ src/main/resources/db/migration/V5__Insert_products.sql
@@ -1,11 +1,10 @@
-INSERT INTO `store_products` VALUES (1,'Kabul','AFG','2022-06-14');
-INSERT INTO `store_products` VALUES (2,'Qandahar','AFG','2022-06-14');
-INSERT INTO `store_products` VALUES (3,'Herat','Herat','2022-06-14');
-INSERT INTO `store_products` VALUES (4,'Mazar-e-Sharif','Balkh','2022-06-14');
-INSERT INTO `store_products` VALUES (5,'Amsterdam','Noord-Holland','2022-06-14');
-INSERT INTO `store_products` VALUES (6,'Rotterdam','Zuid-Holland','2022-06-14');
-INSERT INTO `store_products` VALUES (7,'Haag','NLD','2022-06-14');
-INSERT INTO `store_products` VALUES (8,'Utrecht','Utrecht','2022-06-14');
-INSERT INTO `store_products` VALUES (9,'Eindhoven','Noord-Brabant','2022-06-14');
-INSERT INTO `store_products` VALUES (10,'Tilburg','Noord-Brabant','2022-06-14');
-
+INSERT INTO store_products(name, description, created) VALUES ('NUMBER 1', 'descr 1', SYSDATE());
+INSERT INTO store_products(name, description, created) VALUES ('NUMBER 2', 'descr 1', SYSDATE());
+INSERT INTO store_products(name, description, created) VALUES ('NUMBER 10', 'descr 1', SYSDATE());
+INSERT INTO store_products(name, description, created) VALUES ('NUMBER 6', 'descr 1', SYSDATE());
+INSERT INTO store_products(name, description, created) VALUES ('NUMBER 7', 'descr 1', SYSDATE());
+INSERT INTO store_products(name, description, created) VALUES ('NUMBER 8', 'descr 1', SYSDATE());
+INSERT INTO store_products(name, description, created) VALUES ('NUMBER 3', 'descr 1', SYSDATE());
+INSERT INTO store_products(name, description, created) VALUES ('NUMBER 4', 'descr 1', SYSDATE());
+INSERT INTO store_products(name, description, created) VALUES ('NUMBER 5', 'descr 1', SYSDATE());
+INSERT INTO store_products(name, description, created) VALUES ('NUMBER 9', 'descr 1', SYSDATE());

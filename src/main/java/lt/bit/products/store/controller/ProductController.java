93c082cabbe161dbee42160b4d069f63d5639ff2
AurimasKonst <97383066+AurimasKonst@users.noreply.github.com>
2022-06-21 14:30:10 +0300

    Flyway4

--- src/main/java/lt/bit/products/store/controller/ProductController.java
+++ src/main/java/lt/bit/products/store/controller/ProductController.java
@@ -2,6 +2,7 @@ package lt.bit.products.store.controller;
 
 import java.util.List;
 import lt.bit.products.store.model.Product;
+import lt.bit.products.store.model.ProductItems;
 import lt.bit.products.store.model.ProductRequest;
 import lt.bit.products.store.service.ProductService;
 import org.springframework.http.HttpStatus;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -20,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 class ProductController {
 
   public static final String ROOT_MAPPING = "/products";
+  public static final String ID_MAPPING = "/{id}";
   private final ProductService service;
 
   ProductController(ProductService service) {
@@ -29,15 +32,26 @@ class ProductController {
   @ResponseStatus(HttpStatus.CREATED)
   @PostMapping
   Product createProduct(@RequestBody ProductRequest productRequest) {
-    return null;//service.saveProduct();
+    return service.saveProduct(Product.from(productRequest));
   }
 
-  @GetMapping
+  @PutMapping(ID_MAPPING)
+  ResponseEntity<Product> updateProduct(
+      @RequestBody ProductRequest productRequest,
+      @PathVariable Integer id) {
+    Product product = service.findProduct(id);
+    if (product == null) {
+      return ResponseEntity.notFound().build();
+    }
+    return ResponseEntity.ok(service.saveProduct(Product.from(productRequest, id)));
+  }
+
+  @GetMapping// TODO: add boolean withItems (e.g. /products?withItems=true)
   List<Product> fetchProducts() {
     return service.findProducts();
   }
 
-  @GetMapping("/{id}")
+  @GetMapping(ID_MAPPING)// TODO: add boolean withItems (e.g. /products?withItems=true)
   ResponseEntity<Product> fetchProduct(@PathVariable Integer id) {
     Product product = service.findProduct(id);
     if (product == null) {
@@ -46,7 +60,13 @@ class ProductController {
     return ResponseEntity.ok(product);
   }
 
-  @DeleteMapping("/{id}")
+  @GetMapping(ID_MAPPING + "/items")
+  ResponseEntity<ProductItems> fetchProductItems(@PathVariable("id") Integer productId) {
+    ProductItems items = service.getProductItems(productId);
+    return items == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(items);
+  }
+
+  @DeleteMapping(ID_MAPPING)
   ResponseEntity<Void> deleteProduct(@PathVariable("id") Integer productId) {
     Product product = service.findProduct(productId);
     if (product == null) {
